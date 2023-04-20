4a88827110963eb3573dcb277348fdfa5abaca36
abclf5566 <74696461+abclf5566@users.noreply.github.com>
2023-04-20 17:29:18 +0900

    Update function.py

--- tool/function.py
+++ tool/function.py
@@ -1,7 +1,7 @@
 import okx.PublicData as Public
 
 def get_precision(inst_id):
-    inst_info = publicAPI.get_instruments(instType="SWAP")
+    inst_info = Public.PublicAPI.get_instruments(instType="SWAP")
     for inst in inst_info['data']:
         if inst['instId'] == inst_id:
             tick_sz = inst['tickSz']
