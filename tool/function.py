5638fbfc72b29ab6d2c4156c729d845d78de8eb4
abclf5566 <74696461+abclf5566@users.noreply.github.com>
2023-04-20 17:53:11 +0900

    Update function.py

--- tool/function.py
+++ tool/function.py
@@ -1,6 +1,6 @@
 
-def get_precision(inst_id):
-    inst_info = PublicAPI.get_instruments(instType="SWAP")
+def get_precision(public_api, inst_id):
+    inst_info = public_api.get_instruments(instType="SWAP")
     for inst in inst_info['data']:
         if inst['instId'] == inst_id:
             tick_sz = inst['tickSz']
