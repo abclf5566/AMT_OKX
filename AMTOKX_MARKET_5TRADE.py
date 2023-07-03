6e847b77299d1cbc2adb388777abc70feaa55f95
abclf5566 <74696461+abclf5566@users.noreply.github.com>
2023-07-04 00:39:32 +0900

    Update AMTOKX_MARKET_5TRADE.py

--- AMTOKX_MARKET_5TRADE.py
+++ AMTOKX_MARKET_5TRADE.py
@@ -5,6 +5,7 @@ import okx.PublicData as Public
 from quart import Quart, request
 import tool.function as fn
 from collections import defaultdict
+import asyncio
 
 with open("accinfo.json", "r") as f:
     data = json.load(f)
@@ -30,90 +31,100 @@ instrument_ids = {
 # 初始化交易信息字典
 trade_info = defaultdict(dict)
 
+# 创建异步锁
+lock = asyncio.Lock()
+
 @app.route('/webhook', methods=['POST'])
 async def webhook():
-    
-    data = await request.get_json()
-    if data is None:
-        return {'code': 400, 'message': "Bad Request: Invalid JSON data"}
+    async with lock:
+        data = await request.get_json()
+        if data is None:
+            return {'code': 400, 'message': "Bad Request: Invalid JSON data"}
 
-    direction = data['direction']
-    symbol = data['symbol']
+        direction = data['direction']
+        symbol = data['symbol']
 
-    if symbol not in instrument_ids:
-        return {'code': 400, 'message': f"Bad Request: Invalid symbol {symbol}"}
+        if symbol not in instrument_ids:
+            return {'code': 400, 'message': f"Bad Request: Invalid symbol {symbol}"}
 
-    instrument_id = instrument_ids[symbol]
+        instrument_id = instrument_ids[symbol]
 
-    accountAPI.set_leverage(instId=instrument_id, lever=5, mgnMode='isolated')
+        accountAPI.set_leverage(instId=instrument_id, lever=5, mgnMode='isolated')
 
-    side = None
-    if direction == "Long Entry":
-        side = 'buy'
-    elif direction == "Short Entry":
-        side = 'sell'
+        side = None
+        if direction == "Long Entry":
+            side = 'buy'
+        elif direction == "Short Entry":
+            side = 'sell'
 
-    # 在處理新信號之前檢查當前持倉
-    positions = accountAPI.get_positions(instId=instrument_id)
-    long_position = None
-    short_position = None
+        # 在處理新信號之前檢查當前持倉
+        positions = accountAPI.get_positions(instId=instrument_id)
+        long_position = None
+        short_position = None
 
-    for position in positions['data']:
-        pos = float(position['pos'])
-        if pos > 0:
-            long_position = position
-        elif pos < 0:
-            short_position = position
+        for position in positions['data']:
+            pos = float(position['pos'])
+            if pos > 0:
+                long_position = position
+            elif pos < 0:
+                short_position = position
 
-    # 如果信號方向與當前持倉相同，不執行任何操作
-    if direction == "Long Entry" and long_position is not None:
-        return {'code': 200, 'message': '無需執行操作，持倉方向與信號相符'}
-    elif direction == "Short Entry" and short_position is not None:
-        return {'code': 200, 'message': '無需執行操作，持倉方向與信號相符'}
+        # 如果信號方向與當前持倉相同，不執行任何操作
+        if direction == "Long Entry" and long_position is not None:
+            return {'code': 200, 'message': '無需執行操作，持倉方向與信號相符'}
+        elif direction == "Short Entry" and short_position is not None:
+            return {'code': 200, 'message': '無需執行操作，持倉方向與信號相符'}
 
-    # 修改close_positions函数
-    async def close_positions():
-        if symbol in trade_info:
-            await fn.close_positions_if_exists(instrument_id, tradeAPI, accountAPI, long_position, short_position)
-            trade_info[symbol].clear()
+        # 修改close_positions函数
+        async def close_positions():
+            if symbol in trade_info:
+                close_order = await fn.close_positions_if_exists(instrument_id, tradeAPI, accountAPI, long_position, short_position)
+                # 检查关闭操作是否成功
+                if close_order['code'] != '0':
+                    # 如果关闭操作失败，返回一个错误消息
+                    return {'code': 500, 'message': f"Failed to close positions for symbol {symbol}"}
+                trade_info[symbol].clear()
 
-    if direction == "Exit":
+        # 在"Exit"处理代码中，添加错误检查
+        if direction == "Exit":
+            close_result = await close_positions()
+            if close_result is not None:
+                # 如果关闭操作失败，返回错误消息
+                return close_result
+            return {'code': 201, 'message': "Order EXIT DONE for " + symbol}
+
         await close_positions()
-        return {'code': 201, 'message': "Order EXIT DONE for " + symbol}
-        
-    await close_positions()
 
+        # Get the list of open positions for the specified instruments
+        positions = [accountAPI.get_positions(instId=instrument_id) for instrument_id in instrument_ids.values()]
 
-    # Get the list of open positions for the specified instruments
-    positions = [accountAPI.get_positions(instId=instrument_id) for instrument_id in instrument_ids.values()]
+        # Loop through each trading pair
+        for instrument_id in instrument_ids.values():
+            # Get the max buy for the trading pair
+            trade_size = accountAPI.get_max_order_size(instId=instrument_id, tdMode='isolated')
+            max_buy = trade_size["data"][0]["maxBuy"]
 
-    # Loop through each trading pair
-    for instrument_id in instrument_ids.values():
-        # Get the max buy for the trading pair
-        trade_size = accountAPI.get_max_order_size(instId=instrument_id, tdMode='isolated')
-        max_buy = trade_size["data"][0]["maxBuy"]
+            # Adjust max_buy by a decreasing percentage until the order is successful
+            for i, percentage in enumerate([1, 0.99, 0.98, 0.97, 0.96], 1):
+                adjusted_max_buy = str(int(float(max_buy) * percentage))  # Convert to int as sz only accepts integers
+                order = tradeAPI.place_order(
+                    instId=instrument_id,
+                    tdMode='isolated',
+                    side=side,  # 使用 side 變數
+                    ordType='market',
+                    sz=adjusted_max_buy
+                )
+                if order['code'] == '0':
+                    break
+                elif i == 5:  # If it's the last attempt
+                    raise Exception('Failed to place order after 5 attempts')
 
-        # Adjust max_buy by a decreasing percentage until the order is successful
-        for i, percentage in enumerate([1, 0.99, 0.98, 0.97, 0.96], 1):
-            adjusted_max_buy = str(int(float(max_buy) * percentage))  # Convert to int as sz only accepts integers
-            order = tradeAPI.place_order(
-                instId=instrument_id,
-                tdMode='isolated',
-                side=side,  # 使用 side 變數
-                ordType='market',
-                sz=adjusted_max_buy
-            )
-            if order['code'] == '0':
-                break
-            elif i == 5:  # If it's the last attempt
-                raise Exception('Failed to place order after 5 attempts')
+        trade_info[symbol]["order_id"] = order['data'][0]['ordId']
+        trade_info[symbol]["direction"] = direction
 
-    trade_info[symbol]["order_id"] = order['data'][0]['ordId']
-    trade_info[symbol]["direction"] = direction
+        print(order)
 
-    print(order)
-
-    return {'code': 202, 'message': "Long Entry / Short Entry DONE"}
+        return {'code': 202, 'message': "Long Entry / Short Entry DONE"}
 
 if __name__ == '__main__':
     try:
